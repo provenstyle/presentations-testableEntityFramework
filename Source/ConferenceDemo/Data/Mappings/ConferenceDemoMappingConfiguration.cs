00573a2dbedf82f54385ee000b8c8d94de72bffc
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-12-01 23:48:52 -0600

    finished demo

---
+++ Source/ConferenceDemo/Data/Mappings/ConferenceDemoMappingConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entities;
+using Highway.Data;
+
+namespace Data.Mappings
+{
+    public class ConferenceDemoMappingConfiguration : IMappingConfiguration
+    {
+        public void ConfigureModelBuilder(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Talk>();
+            modelBuilder.Entity<Speaker>();
+        }
+    }
+}
