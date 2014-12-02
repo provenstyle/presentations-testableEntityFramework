b9e72839abb539a582fceca7bb6203052e2d2fb5
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-12-01 22:30:12 -0600

    Staging the demo

--- Source/ConferenceDemo/Web/Models/TalksModel.cs
+++ Source/ConferenceDemo/Web/Models/TalksModel.cs
@@ -1,26 +1,13 @@
-﻿using System.Linq;
-using Data.Queries;
-using Highway.Data;
+﻿using System;
+using System.Linq;
 
 namespace Web.Models
 {
     public class TalksModel
     {
-        readonly IRepository _repository;
-
-        public TalksModel(IRepository repository)
-        {
-            _repository = repository;
-        }
-
         public TalkViewModel GetTalks(int speakerId)
         {
-            var talks = _repository.Find(new TalksBySpeakerId(speakerId)).ToList();
-            return new TalkViewModel
-            {
-                AcceptedTalks = talks.Where(x=> x.Accepted),
-                SubmittedTalks = talks.Where(x=>!x.Accepted)
-            };
+            throw new NotImplementedException();
         }
     }
 }
--- Source/ConferenceDemo/WebTests/TalksModelTests.cs
+++ Source/ConferenceDemo/WebTests/TalksModelTests.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Linq.Dynamic;
 using Data;
 using Data.Entities;
-using Data.Queries;
 using Highway.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -16,27 +15,6 @@ namespace WebTests
     [TestClass]
     public class TalksModelTests
     {
-        [TestMethod]
-        public void returns_accepted_and_non_accepted_talks()
-        {
-            //Arrange
-            var repo = new Mock<IRepository>();
-            repo
-                .Setup(x => x.Find(It.IsAny<TalksBySpeakerId>()))
-                .Returns(new List<Talk>
-                {
-                    new Talk{Accepted = true},
-                    new Talk{Accepted = false}
-                }.AsQueryable());
-
-            var model = new TalksModel(repo.Object);
-
-            //Act
-            var talks = model.GetTalks(1);
-
-            //Assert
-            Assert.AreEqual(DynamicEnumerable.Count(talks.AcceptedTalks), 1);
-            Assert.AreEqual(DynamicEnumerable.Count(talks.SubmittedTalks), 1);
-        }
+        
     }
 }
