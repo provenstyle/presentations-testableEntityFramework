c6ceaf849b83a67e57dbfdc0cd6988a78fec2a4b
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-12-01 22:23:27 -0600

    completed the demo

---
+++ Source/ConferenceDemo/Web/Models/TalksModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Data.Queries;
+using Highway.Data;
+
+namespace Web.Models
+{
+    public class TalksModel
+    {
+        readonly IRepository _repository;
+
+        public TalksModel(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public TalkViewModel GetTalks(int speakerId)
+        {
+            var talks = _repository.Find(new TalksBySpeakerId(speakerId)).ToList();
+            return new TalkViewModel
+            {
+                AcceptedTalks = talks.Where(x=> x.Accepted),
+                SubmittedTalks = talks.Where(x=>!x.Accepted)
+            };
+        }
+    }
+}
---
+++ Source/ConferenceDemo/WebTests/TalksModelTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Dynamic;
+using Data;
+using Data.Entities;
+using Data.Queries;
+using Highway.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Web.Models;
+
+namespace WebTests
+{
+    [TestClass]
+    public class TalksModelTests
+    {
+        [TestMethod]
+        public void returns_accepted_and_non_accepted_talks()
+        {
+            //Arrange
+            var repo = new Mock<IRepository>();
+            repo
+                .Setup(x => x.Find(It.IsAny<TalksBySpeakerId>()))
+                .Returns(new List<Talk>
+                {
+                    new Talk{Accepted = true},
+                    new Talk{Accepted = false}
+                }.AsQueryable());
+
+            var model = new TalksModel(repo.Object);
+
+            //Act
+            var talks = model.GetTalks(1);
+
+            //Assert
+            Assert.AreEqual(DynamicEnumerable.Count(talks.AcceptedTalks), 1);
+            Assert.AreEqual(DynamicEnumerable.Count(talks.SubmittedTalks), 1);
+        }
+    }
+}
